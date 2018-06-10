9e13c8bccdfd8425e891d6c10f8792f9dbada08d
RecherJ <ygliusz@foxmail.com>
2018-06-10 21:06:06 +0800

    初始化需要的文件

---
+++ static/js/main.js
@@ -0,0 +1,11 @@
+// jQuery for page scrolling feature
+jQuery(document).ready(function(e) {
+    e(".scroll").click(function(t) {
+        t.preventDefault();
+        e("html,body").animate({
+            scrollTop: e(this.hash).offset().top
+        }, 1e3)
+    })
+});
+
+
